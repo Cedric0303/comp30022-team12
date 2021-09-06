c4f1a330abd81a6dac77bdc31e97f86d5ff08858
Samantha Min Syuen Goh <sgohminsyuen@student.unimelb.edu.au>
2021-09-06 16:15:57 +0800

    got input from user

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -4,6 +4,21 @@ function Login(props) {
     return (
         <div>
             <h2>Login</h2>
+            
+            <form>
+                <p>Username: <input
+                    type="text"
+                />
+                </p> 
+            </form>
+            <form>
+                <p>Password: <input
+                    type="text"
+                />
+                </p> 
+            </form>
+            <button>Login</button>
+
         </div>
     );
 }
