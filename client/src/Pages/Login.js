553307c16413dd4acddd3da9c5af7f821215bd00
Cedric0303 <chenjunli7@gmail.com>
2021-09-06 18:47:19 +0800

    initial frontend login implementation

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,26 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
+import { BACKEND_API_URL } from "../Constants/Constants.js";
+import axios from "axios";
 
-function Login(props) {
+const Login = (props) => {
+    const [state, setState] = useState({
+        username: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setState((prevState) => ({
+            ...prevState,
+            [id]: value,
+        }));
+    };
+
+    const handleLogin = () => {
+        console.log(state);
+        if (state.username.length && state.password.length) {
+            const payload = {
+                username: state.username,
+                password: state.password,
+            };
+            axios
+                .post(BACKEND_API_URL + "/account/login", payload)
+                .then((response) => {
+                    setState((prevState) => ({
+                        ...prevState,
+                        message: response.data.message,
+                    }));
+                    if (response.status === 200) {
+                        localStorage.setItem(
+                            "accessToken",
+                            response.data.token
+                        );
+                        redirectToHome();
+                    }
+                })
+                .catch(() => {
+                    redirectToLandingPage();
+                });
+        }
+    };
+
+    const redirectToHome = () => {
+        props.history.push("/home");
+    };
+
+    const redirectToLandingPage = () => {
+        props.history.push("/");
+    };
+
     return (
         <div>
-            <h2>Login</h2>
-            
-            <form>
-                <p>Username: <input
-                    type="text"
-                />
-                </p> 
-            </form>
-            <form>
-                <p>Password: <input
-                    type="text"
-                />
-                </p> 
-            </form>
-            <button>Login</button>
-
+            <h2>Login Page</h2>
+            <label>Username: </label>
+            <input
+                type="text"
+                className="form-control"
+                id="username"
+                placeholder="Enter username"
+                value={state.username}
+                onChange={handleChange}
+            />
+            <p />
+            <label>Password: </label>
+            <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+                value={state.password}
+                onChange={handleChange}
+            />
+            <p />
+            <button onClick={handleLogin}>Login</button>
         </div>
     );
-}
+};
 
-export default Login;
+export default withRouter(Login);
