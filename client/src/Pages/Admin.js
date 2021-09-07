514b1b55bf9a12ab5d69677c5f114e5071d902bf
jeongwoo seo <79891380+jwoo2023@users.noreply.github.com>
2021-09-07 13:25:06 +1000

    Update admin page

--- client/src/Pages/Admin.js
+++ client/src/Pages/Admin.js
@@ -10,9 +10,9 @@ function Admin(props) {
         <main>
             <h2 class="mainHeading">Admin</h2>
             <div id="adminButtons">
-                <div class="squareButton"><FontAwesomeIcon icon="user" /></div>
-                <div class="squareButton"><FontAwesomeIcon icon="tag" /></div>
-                <div class="squareButton"><FontAwesomeIcon icon="user-tie" /></div>
+                <a class="squareButton hoverable" href="admin/users"><FontAwesomeIcon class="AdminIcon" icon="user" />Manage Users</a>
+                <a class="squareButton hoverable" href="admin/tags"><FontAwesomeIcon class="AdminIcon" icon="tag" />Manage Tags</a>
+                <a class="squareButton hoverable" href=""><FontAwesomeIcon class="AdminIcon" icon="user-tie" />Manage Clients</a>
             </div>
         </main>
     );
