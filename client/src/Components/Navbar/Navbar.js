81b97be61f9d51e8b3caae3d35da7a285970990f
wangmyj <wangmy@student.unimelb.edu.au>
2021-09-07 10:29:12 +1000

    added active link identification on nav bar

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {NavLink} from 'react-router-dom'
 import "./Navbar.css";
 import logo from "./logo.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -8,7 +9,7 @@ function Navbar(props) {
     // Render admin tab if logged in user is an admin
     const adminNav = () => {
         if (true/*props.user.isAdmin*/) {
-            return <li className="navLeft"><a className="navText" href="/admin">Admin</a></li>
+            return <li className="navLeft"><NavLink activeClassName="active" className="navText" to="/admin">Admin</NavLink></li>
         }
     }
     
@@ -19,9 +20,9 @@ function Navbar(props) {
                 <li className="navLeft"><a href="/home">
                     <img id="logo" src={logo} alt="bobafish logo" />
                 </a></li>
-                <li className="navLeft"><a className="navText" href="/home">Home</a></li>
-                <li className="navLeft"><a className="navText" href="/calendar">Calendar</a></li>
-                <li className="navLeft"><a className="navText" href="/clients">Clients</a></li>
+                <li className="navLeft"><NavLink activeClassName="active" className="navText" to="/home">Home</NavLink></li>
+                <li className="navLeft"><NavLink activeClassName="active" className="navText" to="/calendar">Calendar</NavLink></li>
+                <li className="navLeft"><NavLink activeClassName="active" className="navText" to="/clients">Clients</NavLink></li>
                 {adminNav()}
                 <li className="navRight"><a className="navText" href="/">Sign Out <FontAwesomeIcon icon="sign-out-alt" id="signOut"/></a></li>
             </ul>
