01ca0d06aa945152473a903346819e2a8dfed0f4
zengphyrss <efef.zee@gmail.com>
2021-09-28 23:47:59 +1000

    updated CSS to match mockup, implemented 'Cancel' functionality

--- client/src/Pages/AdminAddUser.js
+++ client/src/Pages/AdminAddUser.js
@@ -55,66 +55,80 @@ function AdminAddUser(props) {
     return (
         <div>
             <Navbar />
-            <h2 className="mainHeading">Admin Add User</h2>
-            <form method="post" onSubmit={onSubmit} className="add-user-form">
-                <div className="field">
-                    <label className="add-user-label">First Name</label>
-                    <input 
-                        className="add-user-input"
-                        required
-                        type="text" 
-                        //name="firstName" 
-                        placeholder="First Name" 
-                        value={firstName}
-                        onChange={e => setFirstName(e.target.value)}
-                    />
+            <h2 className="add-user-heading">Admin Add User</h2>
+            <div className="add-user-container">
+                <div className="add-user-container-item" id="item-left">    
+                    <form method="post" onSubmit={onSubmit} className="add-user-form">
+                        <div className="field-group" id="name-info">
+                            <div className="field">
+                                <label className="add-user-label">First Name:</label>
+                                <input 
+                                    className="add-user-input"
+                                    required
+                                    type="text" 
+                                    //name="firstName" 
+                                    placeholder="First Name" 
+                                    value={firstName}
+                                    onChange={e => setFirstName(e.target.value)}
+                                />
+                            </div>
+                            <div className="field">
+                                <label className="add-user-label">Last Name:</label>
+                                <input 
+                                    className="add-user-input"
+                                    required 
+                                    type="text" 
+                                    //name="lastName" 
+                                    placeholder="Last Name" 
+                                    value={lastName}
+                                    onChange={e => setLastName(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        <div className="field-group" id="login-info">
+                            <div className="field">
+                                <label className="add-user-label">Username:</label>
+                                <input 
+                                    className="add-user-input"
+                                    required
+                                    type="text" 
+                                    //name="username" 
+                                    placeholder="Username" 
+                                    value={username}
+                                    onChange={e => setUsername(e.target.value)}
+                                />
+                            </div>
+                            <div className="field" id="password-field">
+                                <label className="add-user-label">Password:</label>
+                                <input 
+                                    className="add-user-input"
+                                    required
+                                    type={isRevealPwd ? "text" : "password"} 
+                                    //name="password" 
+                                    placeholder="Password" 
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                />
+                                <img 
+                                    style={{height: "40%", width:"auto", paddingLeft:"10px"}}
+                                    title={isRevealPwd ? "Hide Password" : "Show Password"}
+                                    src={isRevealPwd ? hidePwdImg : showPwdImg}
+                                    onClick={() => setIsRevealPwd(!isRevealPwd)}
+                                />
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div className="field">
-                    <label className="add-user-label">Last Name</label>
-                    <input 
-                        className="add-user-input"
-                        required 
-                        type="text" 
-                        //name="lastName" 
-                        placeholder="Last Name" 
-                        value={lastName}
-                        onChange={e => setLastName(e.target.value)}
-                    />
-                </div>
-                <div className="field">
-                    <label className="add-user-label">Username</label>
-                    <input 
-                        className="add-user-input"
-                        required
-                        type="text" 
-                        //name="username" 
-                        placeholder="Username" 
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
-                    />
-                </div>
-                <div className="field" id="password-field">
-                    <label className="add-user-label">Password</label>
-                    <input 
-                        className="add-user-input"
-                        required
-                        type={isRevealPwd ? "text" : "password"} 
-                        //name="password" 
-                        placeholder="Password" 
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                    <img 
-                        style={{height: "40%", width:"auto", paddingLeft:"10px"}}
-                        title={isRevealPwd ? "Hide Password" : "Show Password"}
-                        src={isRevealPwd ? hidePwdImg : showPwdImg}
-                        onClick={() => setIsRevealPwd(!isRevealPwd)}
-                    />
+                <div className="add-user-container-right" id="item-right">
+                    <a className="cancel-option" href="/admin/users">
+                        Cancel
+                    </a>
                 </div>
                 
-                
-            </form>
-            <button className="form-button" onClick={onSubmit}>
+
+            </div>
+            
+            <button className="add-user-button" id="create-user" onClick={onSubmit}>
                 Create User
             </button>
         </div>
