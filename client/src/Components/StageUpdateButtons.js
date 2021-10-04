1738e79df34c8d5da30318eec53280e68b2cee38
jeongwoo seo <79891380+jwoo2023@users.noreply.github.com>
2021-10-05 00:39:35 +1100

    Added stage position update functionality
    - Confirm changes -(renamed to)-> Save changes
    - Saving position changes now sends a post request to the backend
    - Buttons disable correctly while searching

--- client/src/Components/StageUpdateButtons.js
+++ client/src/Components/StageUpdateButtons.js
@@ -3,12 +3,12 @@ import "./css/stageUpdateButtons.css"
 
 // Format each tag
 export default function StageUpdateButtons(props) {
-    let { active } = props;
+    let { active, saveChanges, cancelChanges } = props;
     if (active) {
-        return <div id="updateButtons"><button id="confirmChangesButton">Confirm Changes</button>
-        <button id="cancelChangesButton">Cancel Changes</button></div>
+        return <div id="updateButtons"><button id="saveChangesButton" onClick={saveChanges}>Save Changes</button>
+        <button id="cancelChangesButton" onClick={cancelChanges}>Cancel Changes</button></div>
     } else {
-        return <div id="updateButtons"><button id="confirmChangesButton" disabled>Confirm Changes</button>
-        <button id="cancelChangesButton" disabled>Cancel Changes</button></div>
+        return <div id="updateButtons"><button id="saveChangesButton" onClick={saveChanges} disabled>Save Changes</button>
+        <button id="cancelChangesButton" onClick={cancelChanges} disabled>Cancel Changes</button></div>
     }
 }
