16c9b51d4f9281d033ef3234a2c6d779f9e72491
zengphyrss <efef.zee@gmail.com>
2021-10-05 15:29:48 +1100

    added form and fixed CSS for admin-edit-users

--- client/src/Pages/AdminEditUser.js
+++ client/src/Pages/AdminEditUser.js
@@ -1,11 +1,41 @@
-import React from "react";
-import Navbar from "../Components/Navbar/Navbar.js";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import Navbar from "../Components/Navbar/Navbar.js";
 import "./css/adminEditUser.css";
+import showPwdImg from "./css/show-password.png";
+import hidePwdImg from "./css/hide-password.png";
 
 function AdminEditUser(props) {
 
-    const username = props.match.params.userID;
+    //const username = props.match.params.userID;
+
+    // TODO: Get user from db
+    // const user = {}
+
+    // TODO: Set default values with values from username
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [isRevealPwd, setIsRevealPwd] = useState(false);
+
+    function notEmpty() {
+        if (
+            firstName === "" ||
+            lastName === "" ||
+            username === "" ||
+            password === ""
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    const handleSubmit = () => {
+
+    }
 
     return (
         <div>
@@ -18,6 +48,121 @@ function AdminEditUser(props) {
             </Helmet>
             <Navbar />
             <h2 className="edit-user-heading">Edit User</h2>
+            
+            <div className="edit-user-container">
+                <div className="edit-user-container-item" id="item-left">
+                    <form
+                        method="post"
+                        onSubmit={handleSubmit}
+                        className="edit-user-form"
+                    >
+                        <div className="field-group" id="name-info">
+                            <div className="field">
+                                <label className="edit-user-label">
+                                    First Name:
+                                </label>
+                                <input
+                                    className="edit-user-input"
+                                    required
+                                    type="text"
+                                    placeholder="First Name"
+                                    value={firstName}
+                                    onChange={(e) =>
+                                        setFirstName(e.target.value)
+                                    }
+                                />
+                            </div>
+                            <div className="field">
+                                <label className="edit-user-label">
+                                    Last Name:
+                                </label>
+                                <input
+                                    className="edit-user-input"
+                                    required
+                                    type="text"
+                                    placeholder="Last Name"
+                                    value={lastName}
+                                    onChange={(e) =>
+                                        setLastName(e.target.value)
+                                    }
+                                />
+                            </div>
+                        </div>
+                        <div className="field-group" id="login-info">
+                            <div className="field">
+                                <label className="edit-user-label">
+                                    Username:
+                                </label>
+                                <input
+                                    className="edit-user-input"
+                                    required
+                                    type="text"
+                                    placeholder="Username"
+                                    value={username}
+                                    onChange={(e) =>
+                                        setUsername(e.target.value)
+                                    }
+                                />
+                            </div>
+                            <div className="field" id="password-field">
+                                <label className="edit-user-label">
+                                    Password:
+                                </label>
+                                <input
+                                    className="edit-user-input"
+                                    required
+                                    type={isRevealPwd ? "text" : "password"}
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }
+                                />
+                                <img
+                                    style={{
+                                        height: "40%",
+                                        width: "auto",
+                                        paddingLeft: "10px",
+                                    }}
+                                    title={
+                                        isRevealPwd
+                                            ? "Hide Password"
+                                            : "Show Password"
+                                    }
+                                    src={isRevealPwd ? hidePwdImg : showPwdImg}
+                                    alt={
+                                        isRevealPwd
+                                            ? "Hide Password Icon"
+                                            : "Show Password Icon"
+                                    }
+                                    onClick={() => setIsRevealPwd(!isRevealPwd)}
+                                />
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div className="edit-user-container-item" id="item-right">
+                    <NavLink to="/admin/users" activeClassName="cancel-option">
+                        Cancel
+                    </NavLink>
+                    
+                </div>
+            </div>
+
+            <div className="edit-user-changes">
+                <button
+                    className="edit-user-button"
+                    id="confirm-changes"
+                    onClick={handleSubmit}
+                >
+                    Confirm Changes
+                </button>
+                <NavLink to="/admin/users" activeClassName="delete-option">
+                    Delete User
+                </NavLink>
+            </div>                 
+            
+
         </div>
     );
 }
