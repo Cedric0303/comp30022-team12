1499e56f20e141de1d2d9ee8effc2324699eb900
zengphyrss <efef.zee@gmail.com>
2021-10-05 12:55:11 +1100

    Admin Edit User Page background and heading CSS

--- client/src/Pages/AdminEditUser.js
+++ client/src/Pages/AdminEditUser.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Navbar from "../Components/Navbar/Navbar.js";
+import { Helmet } from "react-helmet";
+import "./css/adminEditUser.css";
 
 function AdminEditUser(props) {
     return (
         <div>
+            <Helmet>
+                <style type="text/css">{`
+                    html {
+                        background-color: #596e80;
+                    }
+                `}</style>
+            </Helmet>
             <Navbar />
-            <h2>Admin Edit User</h2>
+            <h2 className="edit-user-heading">Admin Edit User</h2>
         </div>
     );
 }
