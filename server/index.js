345af6524c1b244987835741d1028b84f53c88c9
Cedric0303 <chenjunli7@gmail.com>
2021-10-06 11:15:48 +0800

    test #3 reload

--- server/index.js
+++ server/index.js
@@ -17,20 +17,13 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
-
 app.use(
     cors({
         credentials: true,
         origin: process.env.REACT_APP_FRONTEND_URL,
     })
 );
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use(express.json());
 
 app.get("/api", (req, res) => {
@@ -68,6 +61,10 @@ app.use(
     passport.authenticate("jwt", { session: false }),
     orderRouter
 );
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
