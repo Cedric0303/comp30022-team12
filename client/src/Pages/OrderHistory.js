daacfbf3adb9e4126cebceeb45a13c5efa78b670
Samantha <sgohminsyuen@student.unimelb.edu.au>
2021-10-10 23:46:54 +0800

    add basic modal code

--- client/src/Pages/OrderHistory.js
+++ client/src/Pages/OrderHistory.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "../Components/Navbar/Navbar.js";
 import OrderRow from "../Components/OrderRow.js";
 import ReactLoading from "react-loading";
 import {useOrders} from "../api.js";
+import Modal from "react-modal";
 
 function OrderHistory(props) {
     const { loading, ordersData, error } = useOrders(props.location.state);
     console.log(ordersData);
+
+     //hold the details of a new order
+     const initialState = {
+        clientReference: "",
+        userReference: "",
+        orderTotal: "",
+    };
+    const [newOrder, setNewOrder] = useState(initialState);
+    const resetOrder = () => {
+     setNewOrder({ ...initialState });
+    };
+
+    Modal.setAppElement(document.getElementById("root") || undefined);
+
+    //handles state of modal's show
+    const [addModalIsOpen, setAddIsOpen] = useState(false);
+    function openModal() {
+        setAddIsOpen(true);
+    }
+    function closeAndClear() {
+        setAddIsOpen(false);
+        resetOrder();
+    }
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+     setNewOrder((prevState) => ({
+            ...prevState,
+            [id]: value,
+        }));
+    };
+
 
     const pageMain = () => {
         if (loading) {
@@ -37,7 +70,7 @@ function OrderHistory(props) {
                         <thead id="orderHistoryHeading">
                             <tr>
                                 <th id="orderID">Order ID</th>
-                                <th id="date">Date</th>
+                                <th id="lastUpdate">Last Updated</th>
                                 <th id="amount">Amount</th>
                             </tr>
                         </thead>
@@ -59,8 +92,42 @@ function OrderHistory(props) {
     return (
         <div>
             <Navbar />
-            <h2>Order History</h2>
-            <main>{pageMain()}</main>
+
+            <main className="OrderBox">
+                <h2 id="orderHeading">Order History</h2>
+                <button onClick={openModal}>Add Order</button>
+                {pageMain()}
+                
+            </main>
+
+            <Modal
+                isOpen={addModalIsOpen}
+                className="addOrderModal"
+                contentLabel="Add New Order"
+            >
+                <h2>Add a new order</h2>
+                <form>
+                    <label>
+                        Order Amount:
+                        <input
+                            type="text"
+                            id="orderTotal"
+                            required
+                            value={newOrder.orderTotal}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <button type="submit" className="addOrderButton">
+                        Add Order
+                    </button>
+                    <button
+                        className="orderCancelButton"
+                        onClick={closeAndClear}
+                    >
+                        Cancel
+                    </button>
+                </form>
+            </Modal>
         </div>
     );
 }
